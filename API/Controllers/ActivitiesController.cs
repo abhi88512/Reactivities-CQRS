3091a2acbe631658ad55e80d549b52080ab5dd7a
abhi88512 <abhilashmohanty2@gmail.com>
2025-07-28 06:05:04 +0530

    feat: Add Mediator Pattern

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,29 +1,26 @@
 using System;
+using Application.Activities.Quries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController() : BaseApiController
 {
     [HttpGet]
 
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
 
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var result = await context.Activities.FindAsync(id);
 
-        if (result == null) return NotFound();
-
-        return Ok(result);
+        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
 
     }
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Quries;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,6 +12,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors();
+
+builder.Services.AddMediatR( x => x.RegisterServicesFromAssemblyContaining
+ <GetActivityList.Handler>());
 
 
 
